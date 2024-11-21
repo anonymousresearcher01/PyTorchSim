3da3759168d5353c20f28f77657c299c7b7c94fa
wonhyuk yang <wonhyuk@postech.ac.kr>
2024-11-21 18:49:01 +0000

    [Utility] upload chiplet script

---
+++ scripts/chiplet.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ -z "$TORCHSIM_DIR" ]; then
+    echo "Error: TORCHSIM_DIR environment variable is not set."
+    exit 1
+fi
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 GEMM_PATH [ATTRIBUTE_FILE]"
+    echo "  GEMM_PATH: Path to the gemm directory (e.g., ../../gemmx1024x1024x1024)"
+    echo "  ATTRIBUTE_FILE: Optional path to the attribute file"
+    exit 1
+fi
+
+GEMM_PATH="$1"
+SIMULATOR_PATH="$TORCHSIM_DIR/PyTorchSimBackend/build/bin/Simulator"
+
+CONFIG_LIST=(
+    "$TORCHSIM_DIR/PyTorchSimBackend/configs/systolic_ws_128x128_c2_simple_noc_tpuv2.json"
+    "$TORCHSIM_DIR/PyTorchSimBackend/configs/systolic_ws_128x128_c2_booksim_tpuv2.json"
+    "$TORCHSIM_DIR/PyTorchSimBackend/configs/systolic_ws_128x128_c2_chiplet_tpuv2.json"
+)
+
+ATTRIBUTE_FILE=""
+if [ $# -eq 2 ]; then
+    ATTRIBUTE_FILE="$GEMM_PATH/attribute/$2"
+    if [ ! -f "$ATTRIBUTE_FILE" ]; then
+        echo "Error: Attribute file '$ATTRIBUTE_FILE' does not exist."
+        exit 1
+    fi
+    echo "Using attribute file: $ATTRIBUTE_FILE"
+else
+    echo "No attribute file provided."
+    exit 1
+fi
+
+RESULTS_DIR="./results/$2"
+mkdir -p "$RESULTS_DIR"
+
+MODELS_LIST="$GEMM_PATH/tile_graph.onnx"
+
+for CONFIG in "${CONFIG_LIST[@]}"; do
+    CONFIG_NAME=$(basename "$CONFIG" .json)
+
+    OUTPUT_FILE="$RESULTS_DIR/${CONFIG_NAME}_result.txt"
+
+    # Run Simulator
+    echo "$SIMULATOR_PATH" --config "$CONFIG" --models_list "$MODELS_LIST" --attributes_list "$ATTRIBUTE_FILE"
+      "$SIMULATOR_PATH" --config "$CONFIG" --models_list "$MODELS_LIST" --log_level trace --attributes_list "$ATTRIBUTE_FILE" > "$OUTPUT_FILE"
+
+    echo "===== Simulation for $CONFIG completed. Results saved to $OUTPUT_FILE ====="
+done
