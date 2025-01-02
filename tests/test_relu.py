ad59f700366d0f71c2169938a8f440f64d8169ca
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2025-01-02 10:45:33 +0000

    [Test] Add relu test case

---
+++ tests/test_relu.py
@@ -0,0 +1,36 @@
+import torch
+import torch._dynamo
+import torch.utils.cpp_extension
+
+def test_result(name, out, cpu_out, rtol=1e-4, atol=1e-4):
+    message = f"|{name} Test Passed|"
+    if torch.allclose(out.cpu(), cpu_out, rtol=rtol, atol=atol):
+        print("-" * len(message))
+        print(message)
+        print("-" * len(message))
+    else:
+        print("custom out: ", out.cpu())
+        print("cpu out: ", cpu_out)
+        exit(1)
+
+def test_ReLU(device, size=(128, 128)):
+    torch.manual_seed(0)
+    input = torch.randn(size)
+    x1 = input.to(device=device)
+    x2 = input.to("cpu")
+    opt_fn = torch.compile(dynamic=False)(torch.nn.functional.relu)
+    y = opt_fn(x1)
+    cpu_y = torch.nn.functional.relu(x2)
+    test_result("ReLU", y, cpu_y)
+
+if __name__ == "__main__":
+    import os
+    import sys
+    sys.path.append(os.path.abspath("/workspace/PyTorchSim"))
+
+    from Scheduler.scheduler import ExecutionEngine
+    module = ExecutionEngine.setup_device()
+    device = module.custom_device()
+    test_ReLU(device, (47, 10))
+    test_ReLU(device, (128, 128))
+    test_ReLU(device, (4071, 429))
