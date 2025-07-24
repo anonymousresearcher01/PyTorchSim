bde2ca45ea1885f729a7492e5d374389d037b36f
Wonhyuk Yang <wonhyuk@postech.ac.kr>
2025-07-24 13:51:17 +0000

    [Test] Add ResNet50 test case

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -1,7 +1,8 @@
+import argparse
 import torch
 import torch._dynamo
 import torch.utils.cpp_extension
-from torchvision.models import resnet18
+from torchvision.models import resnet18, resnet50
 
 def test_result(name, out, cpu_out, rtol=1e-4, atol=1e-4):
     if torch.allclose(out.cpu(), cpu_out, rtol=rtol, atol=atol):
@@ -18,11 +19,16 @@ def test_result(name, out, cpu_out, rtol=1e-4, atol=1e-4):
         print("cpu out: ", cpu_out)
         exit(1)
 
-def test_resnet(device, batch=1):
+def test_resnet(device, batch=1, model_type='resnet18'):
     from torchvision.models import resnet
     with torch.no_grad():
         #model = resnet._resnet(resnet.BasicBlock, [1, 1, 1, 1], weights=None, progress=False).eval()
-        model = resnet18().eval()
+        if model_type == 'resnet50':
+            model = resnet50().eval()
+        elif model_type == 'resnet18':
+            model = resnet18().eval()
+        else:
+            raise ValueError(f"Unsupported model type: {model_type}")
         model.to(device, memory_format=torch.channels_last)
         input = torch.randn(batch, 3, 224, 224)
         x1 = input.to(device=device, memory_format=torch.channels_last)
@@ -31,16 +37,19 @@ def test_resnet(device, batch=1):
         res = opt_fn(x1)
         cpu_model = model.cpu().to(memory_format=torch.channels_last)
         cpu_res = cpu_model(x2)
-    test_result("ResNet18 inference", res, cpu_res)
+    test_result(f"{model_type} inference", res, cpu_res)
     print("Max diff > ", torch.max(torch.abs(res.cpu() - cpu_res)))
-    print("ResNet18 Simulation Done")
+    print(f"{model_type} Simulation Done")
 
 if __name__ == "__main__":
     import os
     import sys
+    args = argparse.ArgumentParser()
+    args.add_argument('--model_type', type=str, default="resnet18", help='ex) resnet18')
+    args = args.parse_args()
     sys.path.append(os.environ.get('TORCHSIM_DIR', default='/workspace/PyTorchSim'))
 
     from Scheduler.scheduler import ExecutionEngine
     module = ExecutionEngine.setup_device()
     device = module.custom_device()
-    test_resnet(device)
+    test_resnet(device, model_type=args.model_type)
