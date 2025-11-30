0d5745c92655ea98c5de08a3460b94dfcb173275
OkkyunWoo <okkyun.w@postech.ac.kr>
2025-11-30 16:51:45 +0000

    Fix method of getting param from os env

--- PyTorchSimFrontend/extension_config.py
+++ PyTorchSimFrontend/extension_config.py
@@ -3,78 +3,124 @@ import sys
 import tempfile
 import importlib
 
-# Hardware info config
-CONFIG_VECTOR_LANE = int(os.environ.get("TORCHSIM_VECTOR_LANE", default=128))
-CONFIG_VECTOR_LANE_STRIDE = int(os.environ.get("TORCHSIM_VECTOR_LANE_STRIDE", default=2))
-CONFIG_SPAD_INFO = {
-  "spad_vaddr" : 0xD0000000,
-  "spad_paddr" : 0x2000000000,
-  "spad_size" : int(os.environ.get("TORCHSIM_SPAD_SIZE", default=128)) << 10 # Note: spad size per lane
-}
-CONFIG_PRECISION = 4 # 32bit
-CONFIG_NUM_CORES = 1
-CONFIG_VLEN = 256 # 256bits / 32bits = 8 [elements]
+def __getattr__(name):
 
-# Tile size config
-CONFIG_TORCHSIM_DIR = os.environ.get('TORCHSIM_DIR', default='/workspace/PyTorchSim')
+    # Hardware info config
+    if name == "CONFIG_VECTOR_LANE":
+        return int(os.environ.get("TORCHSIM_VECTOR_LANE", default=128))
+    if name == "CONFIG_VECTOR_LANE_STRIDE":
+        return int(os.environ.get("TORCHSIM_VECTOR_LANE_STRIDE", default=2))
+    if name == "CONFIG_SPAD_INFO":
+        return {
+          "spad_vaddr" : 0xD0000000,
+          "spad_paddr" : 0x2000000000,
+          "spad_size" : int(os.environ.get("TORCHSIM_SPAD_SIZE", default=128)) << 10 # Note: spad size per lane
+        }
+    if name == "CONFIG_PRECISION":
+          return 4 # 32bit
+    if name == "CONFIG_NUM_CORES":
+          return 1
+    if name == "CONFIG_VLEN":
+          return 256 # 256bits / 32bits = 8 [elements]
 
-CONFIG_TORCHSIM_DUMP_PATH = os.environ.get('TORCHSIM_DUMP_PATH',
-                        default = f"{tempfile.gettempdir()}/torchinductor")
-CONFIG_TORCHSIM_DUMP_FILE = int(os.environ.get('TORCHSIM_DUMP_FILE', default=True))
-CONFIG_TORCHSIM_FUNCTIONAL_MODE = int(os.environ.get('TORCHSIM_FUNCTIONAL_MODE', default=True))
-CONFIG_TORCHSIM_TIMING_MODE = int(os.environ.get("TORCHSIM_TIMING_MODE", True))
-CONFIG_CLEANUP_DUMP_ARGS = int(os.environ.get('CLEANUP_DUMP_ARGS', default=False))
+    # Tile size config
+    if name == "CONFIG_TORCHSIM_DIR":
+          return os.environ.get('TORCHSIM_DIR', default='/workspace/PyTorchSim')
 
-# LLVM PATH
-CONFIG_TORCHSIM_LLVM_PATH = os.environ.get('TORCHSIM_LLVM_PATH', default="/usr/bin")
-CONFIG_TORCHSIM_CUSTOM_PASS_PATH = os.environ.get('TORCHSIM_CUSTOM_PASS_PATH',
-                                           default=f"{CONFIG_TORCHSIM_DIR}/GemminiLowerPass/build")
-CONFIG_TORCHSIM_DUMP_MLIR_IR = int(os.environ.get("TORCHSIM_DUMP_MLIR_IR", default=False))
-CONFIG_TORCHSIM_DUMP_LLVM_IR = int(os.environ.get("TORCHSIM_DUMP_LLVM_IR", default=False))
+    if name == "CONFIG_TORCHSIM_DUMP_PATH":
+          return os.environ.get('TORCHSIM_DUMP_PATH', default = f"{tempfile.gettempdir()}/torchinductor")
+    if name == "CONFIG_TORCHSIM_DUMP_FILE":
+          return int(os.environ.get('TORCHSIM_DUMP_FILE', default=True))
+    if name == "CONFIG_TORCHSIM_FUNCTIONAL_MODE":
+          return int(os.environ.get('TORCHSIM_FUNCTIONAL_MODE', default=True))
+    if name == "CONFIG_TORCHSIM_TIMING_MODE":
+          return int(os.environ.get("TORCHSIM_TIMING_MODE", True))
+    if name == "CONFIG_CLEANUP_DUMP_ARGS":
+          return int(os.environ.get('CLEANUP_DUMP_ARGS', default=False))
 
-# TOGSim config
-CONFIG_TOGSIM_CONFIG = os.environ.get('TORCHSIM_CONFIG',
-                                        default=f'{CONFIG_TORCHSIM_DIR}/TOGSim/configs/systolic_ws_128x128_c1_simple_noc_tpuv3.json')
-CONFIG_TOGSIM_EAGER_MODE = int(os.environ.get("TOGSIM_EAGER_MODE", default=False))
-CONFIG_TOGSIM_DRYRUN = int(os.environ.get('TOGSIM_DRYRUN', default=False))
-CONFIG_TOGSIM_DEBUG_LEVEL = os.environ.get("TOGSIM_DEBUG_LEVEL", "")
+    # LLVM PATH
+    if name == "CONFIG_TORCHSIM_LLVM_PATH":
+        return os.environ.get('TORCHSIM_LLVM_PATH', default="/usr/bin")
+    if name == "CONFIG_TORCHSIM_CUSTOM_PASS_PATH":
+        return os.environ.get('TORCHSIM_CUSTOM_PASS_PATH',
+                                              default=f"{__getattr__('CONFIG_TORCHSIM_DIR')}/GemminiLowerPass/build")
+    if name == "CONFIG_TORCHSIM_DUMP_MLIR_IR":
+        return int(os.environ.get("TORCHSIM_DUMP_MLIR_IR", default=False))
+    if name == "CONFIG_TORCHSIM_DUMP_LLVM_IR":
+        return int(os.environ.get("TORCHSIM_DUMP_LLVM_IR", default=False))
 
-# GEM5 config
-CONFIG_GEM5_PATH = os.environ.get('GEM5_PATH', default="/workspace/gem5/build/RISCV/gem5.opt")
-CONFIG_GEM5_SCRIPT_PATH = os.environ.get('GEM5_SCRIPT_PATH',
-                                  default=f"{CONFIG_TORCHSIM_DIR}/gem5_script/script_systolic.py")
+    # TOGSim config
+    if name == "CONFIG_TOGSIM_CONFIG":
+        return os.environ.get('TORCHSIM_CONFIG',
+                default=f"{__getattr__('CONFIG_TORCHSIM_DIR')}/TOGSim/configs/systolic_ws_128x128_c1_simple_noc_tpuv3.json")
+    if name == "CONFIG_TOGSIM_EAGER_MODE":
+        return int(os.environ.get("TOGSIM_EAGER_MODE", default=False))
+    if name == "CONFIG_TOGSIM_DRYRUN":
+        return int(os.environ.get('TOGSIM_DRYRUN', default=False))
+    if name == "CONFIG_TOGSIM_DEBUG_LEVEL":
+        return os.environ.get("TOGSIM_DEBUG_LEVEL", "")
 
-# AUTOTUNE config
-CONFIG_AUTOTUNE = int(os.environ.get('AUTOTUNE', default=False))
-CONFIG_AUTOTUNE_TEMPLATE = int(os.environ.get('AUTOTUNE_TEMPLATE', default=False))
-CONFIG_MAX_AUTOTUNE_TRY = int(os.environ.get('MAX_AUTOTUNE_TRY', default=10))
-CONFIG_AUTOTUNE_TEMPLATE_TOPK = int(os.environ.get('AUTOTUNE_TEMPLATE_TOPK', default=4))
+    # GEM5 config
+    if name == "CONFIG_GEM5_PATH":
+        return os.environ.get('GEM5_PATH', default="/workspace/gem5/build/RISCV/gem5.opt")
+    if name == "CONFIG_GEM5_SCRIPT_PATH":
+        return os.environ.get('GEM5_SCRIPT_PATH',
+                                      default=f"{__getattr__('CONFIG_TORCHSIM_DIR')}/gem5_script/script_systolic.py")
 
-# For block sparse
-CONFIG_BLOCK_SPARSE = int(os.environ.get('BLOCK_SPARSE', default=0))
+    # AUTOTUNE config
+    if name == "CONFIG_AUTOTUNE":
+        return int(os.environ.get('AUTOTUNE', default=False))
+    if name == "CONFIG_AUTOTUNE_TEMPLATE":
+        return int(os.environ.get('AUTOTUNE_TEMPLATE', default=False))
+    if name == "CONFIG_MAX_AUTOTUNE_TRY":
+        return int(os.environ.get('MAX_AUTOTUNE_TRY', default=10))
+    if name == "CONFIG_AUTOTUNE_TEMPLATE_TOPK":
+        return int(os.environ.get('AUTOTUNE_TEMPLATE_TOPK', default=4))
 
-# For GEMM tile size
-CONFIG_MANUAL_TILE_SIZE = int(os.environ.get('TORCHSIM_MANUAL_TILE_SIZE', default=False))
-CONFIG_TILE_M = int(os.environ.get('TORCHSIM_TILE_M', default=CONFIG_VECTOR_LANE))
-CONFIG_TILE_N = int(os.environ.get('TORCHSIM_TILE_N', default=CONFIG_VECTOR_LANE))
-CONFIG_TILE_K = int(os.environ.get('TORCHSIM_TILE_K', default=CONFIG_VECTOR_LANE))
-CONFIG_GEMM_CHEATSHEET_PATH = os.environ.get('TORCHSIM_GEMM_CHEATSHEET_PATH',
-                                            default=f"{CONFIG_TORCHSIM_DIR}/validation/gemm_tpuv3_cheatsheet.json")
-CONFIG_SUBTILE = int(os.environ.get('TORCHSIM_SUBTILE', default=True))
-CONFIG_MANUAL_SUBTILE_SIZE = int(os.environ.get('TORCHSIM_MANUAL_SUBTILE_SIZE', default=False))
-CONFIG_SUBTILE_M = int(os.environ.get('TORCHSIM_SUBTILE_M', default=CONFIG_VECTOR_LANE))
-CONFIG_SUBTILE_N = int(os.environ.get('TORCHSIM_SUBTILE_N', default=CONFIG_VECTOR_LANE))
-CONFIG_SUBTILE_K = int(os.environ.get('TORCHSIM_SUBTILE_K', default=CONFIG_VECTOR_LANE))
+    # For block sparse
+    if name == "CONFIG_BLOCK_SPARSE":
+        return int(os.environ.get('BLOCK_SPARSE', default=0))
 
-# Compiler Optimization
-CONFIG_COMPILER_OPTIMIZATION = os.environ.get('TORCHSIM_COMPILER_OPTIMIZATION', default="all")  # options: all, none, custom
-# Advanced fusion options
-CONFIG_FUSION = True if (CONFIG_COMPILER_OPTIMIZATION == "all" or "fusion" in CONFIG_COMPILER_OPTIMIZATION) else False
-CONFIG_FUSION_REDUCTION_EPILOGUE = True if (CONFIG_COMPILER_OPTIMIZATION == "all" or "reduction_epliogue" in CONFIG_COMPILER_OPTIMIZATION) else False
-CONFIG_FUSION_REDUCTION_REDUCTION = True if (CONFIG_COMPILER_OPTIMIZATION == "all" or "reduction_reduction" in CONFIG_COMPILER_OPTIMIZATION) else False
-CONFIG_FUSION_PROLOGUE = True if ((CONFIG_COMPILER_OPTIMIZATION == "all") or ("prologue" in CONFIG_COMPILER_OPTIMIZATION)) else False
-CONFIG_SINGLE_BATCH_CONV = True if (CONFIG_COMPILER_OPTIMIZATION == "all" or "single_batch_conv" in CONFIG_COMPILER_OPTIMIZATION) else False
-CONFIG_MULTI_TILE_CONV = True if (CONFIG_COMPILER_OPTIMIZATION == "all" or "multi_tile_conv" in CONFIG_COMPILER_OPTIMIZATION) else False
+    # For GEMM tile size
+    if name == "CONFIG_MANUAL_TILE_SIZE":
+        return int(os.environ.get('TORCHSIM_MANUAL_TILE_SIZE', default=False))
+    if name == "CONFIG_TILE_M":
+        return int(os.getenv("TORCHSIM_TILE_M", __getattr__("CONFIG_VECTOR_LANE")))
+    if name == "CONFIG_TILE_N":
+        return int(os.getenv("TORCHSIM_TILE_N", __getattr__("CONFIG_VECTOR_LANE")))
+    if name == "CONFIG_TILE_K":
+        return int(os.getenv("TORCHSIM_TILE_K", __getattr__("CONFIG_VECTOR_LANE")))
+
+    if name == "CONFIG_SUBTILE":
+        return int(os.environ.get('TORCHSIM_SUBTILE', default=True))
+    if name == "CONFIG_MANUAL_SUBTILE_SIZE":
+        return int(os.environ.get('TORCHSIM_MANUAL_SUBTILE_SIZE', default=False))
+    if name == "CONFIG_SUBTILE_M":
+        return int(os.environ.get('TORCHSIM_SUBTILE_M', default=__getattr__("CONFIG_VECTOR_LANE")))
+    if name == "CONFIG_SUBTILE_N":
+        return int(os.environ.get('TORCHSIM_SUBTILE_N', default=__getattr__("CONFIG_VECTOR_LANE")))
+    if name == "CONFIG_SUBTILE_K":
+        return int(os.environ.get('TORCHSIM_SUBTILE_K', default=__getattr__("CONFIG_VECTOR_LANE")))
+
+    if name == "CONFIG_GEMM_CHEATSHEET_PATH":
+          return os.environ.get('TORCHSIM_GEMM_CHEATSHEET_PATH',
+                          default=f"{__getattr__('CONFIG_TORCHSIM_DIR')}/validation/gemm_tpuv3_cheatsheet.json")
+    # Compiler Optimization
+    if name == "CONFIG_COMPILER_OPTIMIZATION":
+          return os.environ.get('TORCHSIM_COMPILER_OPTIMIZATION', default="all")  # options: all, none, custom
+    # Advanced fusion options
+    if name == "CONFIG_FUSION":
+          return True if (__getattr__("CONFIG_COMPILER_OPTIMIZATION") == "all" or "fusion" in __getattr__("CONFIG_COMPILER_OPTIMIZATION")) else False
+    if name == "CONFIG_FUSION_REDUCTION_EPILOGUE":
+          return True if (__getattr__("CONFIG_COMPILER_OPTIMIZATION") == "all" or "reduction_epliogue" in __getattr__("CONFIG_COMPILER_OPTIMIZATION")) else False
+    if name == "CONFIG_FUSION_REDUCTION_REDUCTION":
+          return True if (__getattr__("CONFIG_COMPILER_OPTIMIZATION") == "all" or "reduction_reduction" in __getattr__("CONFIG_COMPILER_OPTIMIZATION")) else False
+    if name == "CONFIG_FUSION_PROLOGUE":
+          return True if ((__getattr__("CONFIG_COMPILER_OPTIMIZATION") == "all") or ("prologue" in __getattr__("CONFIG_COMPILER_OPTIMIZATION"))) else False
+    if name == "CONFIG_SINGLE_BATCH_CONV":
+          return True if (__getattr__("CONFIG_COMPILER_OPTIMIZATION") == "all" or "single_batch_conv" in __getattr__("CONFIG_COMPILER_OPTIMIZATION")) else False
+    if name == "CONFIG_MULTI_TILE_CONV":
+          return True if (__getattr__("CONFIG_COMPILER_OPTIMIZATION") == "all" or "multi_tile_conv" in __getattr__("CONFIG_COMPILER_OPTIMIZATION")) else False
 
 # SRAM Buffer allocation plan
 def load_plan_from_module(module_path):
